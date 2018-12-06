c88873d2a6fb0dbd0bfd60767482c154e645e2a3
Snivell <37510691+Snivell@users.noreply.github.com>
2018-12-06 16:30:34 +0100

    ajout de deux autres malus

---
+++ metier/malus/SoustraitValeursDouble.java
@@ -0,0 +1,15 @@
+package projet.metier.malus;
+
+import projet.metier.Carte;
+import projet.metier.ConvertitHauteurValeur;
+import projet.metier.Malus;
+
+public class SoustraitValeursDouble extends Malus {
+	
+	public int calculeValeur(Carte carte1, Carte carte2){
+        return 
+        -2 * (ConvertitHauteurValeur.convertit(carte1.getHauteur())
+         + ConvertitHauteurValeur.convertit(carte2.getHauteur()));
+    }
+    
+}
