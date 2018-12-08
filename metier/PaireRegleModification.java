cd01721152fa3ae442f39a648bd67c253ec2fcf2
Snivell <vincent.gindt@gmail.com>
2018-12-08 21:09:35 +0100

    Ajout numero a la paire de regle modificateur pour un attribut visuel du jeu

--- metier/PaireRegleModification.java
+++ metier/PaireRegleModification.java
@@ -2,10 +2,12 @@ package projet.metier;
 
 public class PaireRegleModification{
 
+	public int numero;
     public Regle regle;
     public ModificationScore modif;
 
-    public PaireRegleModification(Regle regle, ModificationScore modif){
+    public PaireRegleModification(int num, Regle regle, ModificationScore modif){
+    	this.numero = num;
         this.regle = regle;
         this.modif = modif;
     }    
