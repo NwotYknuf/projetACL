13f19769997b57976b82afeb17fb853bbc18a584
Snivell <vincent.gindt@gmail.com>
2018-12-08 18:36:39 +0100

    test sur le pseudo -> que des lettres et numéros

--- vue/ConfirmerListener.java
+++ vue/ConfirmerListener.java
@@ -1,6 +1,7 @@
 package projet.vue;
 
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ConfirmerListener implements ActionListener{
 
@@ -13,11 +14,17 @@ public class ConfirmerListener implements ActionListener{
     public void actionPerformed(ActionEvent e){
     	
     	String pseudo = fenetrePseudo.getPseudo();
+    	
+    	String regExpression = "[a-zA-Z_0-9]*"; 
+        
         //Verifs sur le pseudo
-    	if (pseudo.length() < 1 || pseudo.length() > 20) {
+    	if (pseudo.length() < 1 || pseudo.length() > 12) { // Test si le pseudo est nul ou supérieur à 12 caractères
     		System.out.println("Pseudo invalide");
     	}
-    	else { //Creer une nouvelle fenetre de jeux
+    	else if (!pseudo.matches(regExpression)) { // Test si le pseudo contient autre chose que des lettres et numéro
+    		System.out.println("Pseudo invalide");
+    	}
+    	else { //Le pseudo est correct, créer une nouvelle fenetre de jeux
     		new FenetreJeu(pseudo);
     		fenetrePseudo.dispose();
     	}
