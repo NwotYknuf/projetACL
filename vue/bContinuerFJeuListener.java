6c53d6782a86515ec87a5c8d7d90780941023021
Snivell <vincent.gindt@gmail.com>
2018-12-09 01:46:51 +0100

    image de felicitations au lieu de font

--- vue/bContinuerFJeuListener.java
+++ vue/bContinuerFJeuListener.java
@@ -1,15 +1,19 @@
 package projet.vue;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.GraphicsEnvironment;
 import java.awt.Label;
+import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class bContinuerFJeuListener implements ActionListener {
 	
@@ -33,15 +37,20 @@ public class bContinuerFJeuListener implements ActionListener {
     	fenetre.getlPointsTour().setVisible(false);
     	fenetre.getlScore().setVisible(false);
     	
-    	//afficher felicitation
+    	//afficher félicitations
+    	Panel pBravo = new Panel();
+    	pBravo.setBounds(40, 110, 260, 120);
+    	pBravo.add(new ComposantImageAWT("/projet/ressources/bravo.jpg", 260, 120));
+        fenetre.add(pBravo);
     	
+    	/*
     	Label lFelicitation = new Label("Félicitations !");
 		lFelicitation.setBounds(60, 120, 100, 40);
 		fenetre.add(lFelicitation);
     	
 		try {
 			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			Font font = new Font(Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("/projet/ressources/Black-Melody.otf"))).getFamily(), Font.BOLD, 35);
+			Font font = new Font(Font.createFont(Font.TRUETYPE_FONT, new FileInputStream(new File("/projet/ressources/Caruban.ttf"))).getFamily(), Font.BOLD, 35);
 			ge.registerFont(font);
 			lFelicitation.setFont(font);
 			
@@ -54,11 +63,18 @@ public class bContinuerFJeuListener implements ActionListener {
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}
+		}*/
     	
     	//afficher scorefinal
-    	Label lScoreFinal = new Label("Votre score final est de : " + fenetre.getPartie().getScoreFinal() + " points.");
-    	lScoreFinal.setBounds(60, 180, 300, 40);
+        Label lScore = new Label("Votre score final est de : ");
+        lScore.setFont(new Font("Courier New", Font.PLAIN, 12));
+        lScore.setBounds(60, 250, 200, 15);
+    	fenetre.add(lScore);
+    	
+    	Label lScoreFinal = new Label(fenetre.getPartie().getScoreFinal() + " points.");
+    	lScoreFinal.setBounds(140, 265, 100, 40);
+    	lScoreFinal.setFont(new Font("Times New Roman", Font.BOLD, 20));
+    	lScoreFinal.setForeground(Color.BLUE);
     	fenetre.add(lScoreFinal);
     	
     	fenetre.getbRetour().setEnabled(true);
